09ed7eb8a9b2ba2e6b2037a26c2683a66da0d923
roosevejun <roosevejun@gmail.com>
2014-09-17 10:41:36 +0800

    增加监理考试成绩查询

--- src/main/java/com/tongtu/controller/zyzg/QueryVExamSupeCharge.java
+++ src/main/java/com/tongtu/controller/zyzg/QueryVExamSupeCharge.java
@@ -1,4 +1,4 @@
-package com.tongtu.controller.publics;
+package com.tongtu.controller.zyzg;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
